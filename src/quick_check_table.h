1e7e4782f75d299f4854240e3ffd97d17dbb5989
Andreas Weber <weber@alluris.de>
2016-01-18 10:37:36 +0100

    New ttt_quick_check_config

---
+++ src/quick_check_table.h
@@ -0,0 +1,48 @@
+#include <FL/Fl.H>
+#include <FL/Fl_Table.H>
+#include <FL/fl_draw.H>
+#include <iostream>
+#include <vector>
+
+#ifndef _QUICK_CHECK_TABLE_
+#define _QUICK_CHECK_TABLE_
+
+using namespace std;
+
+class quick_check_table : public Fl_Table
+{
+private:
+  double nominal_value;
+  vector <double> peak_values;
+
+  void DrawHeader(const char *s, int X, int Y, int W, int H);
+  void DrawData(const char *s, int X, int Y, int W, int H, Fl_Color bgcolor);
+  void draw_cell(TableContext context, int ROW=0, int COL=0, int X=0, int Y=0, int W=0, int H=0);
+
+public:
+
+  quick_check_table(int X, int Y, int W, int H, const char *L=0);
+  ~quick_check_table();
+
+  void clear ()
+  {
+    peak_values.clear ();
+  }
+
+  void set_nominal_value (double nom)
+  {
+    nominal_value = nom;
+  }
+
+  void add_measurement (double v)
+  {
+    peak_values.push_back (v);
+    rows (peak_values.size ());
+    redraw ();
+    cout << "add_measurement v=" << v << " size=" << peak_values.size () << endl;
+
+  }
+
+};
+
+#endif
---
+++ src/ttt_quick_check_config.f
@@ -0,0 +1,135 @@
+# data file for the Fltk User Interface Designer (fluid)
+version 1.0303 
+header_name {.h} 
+code_name {.cxx}
+decl {\#include <FL/fl_ask.H>} {public global
+} 
+
+decl {\#include "liballuris++.h"} {public global
+} 
+
+decl {\#include "quick_check_table.h"} {public global
+} 
+
+Function {} {open
+} {
+  Fl_Window {} {
+    label {TTT quick check config} open
+    xywh {2464 349 530 645} type Double visible
+  } {
+    Fl_Value_Input vi_nominal {
+      label {Nominalwert [Nm]}
+      xywh {210 20 55 25} minimum -100 maximum 100 step 0.1
+    }
+    Fl_Value_Input vi_upper_limit {
+      label {oberer Grenzwert [Nm]}
+      xywh {210 70 55 25} minimum -100 maximum 100 step 0.01
+    }
+    Fl_Value_Input vi_lower_limit {
+      label {unterer Grenzwert [Nm]}
+      xywh {210 100 55 25} minimum -100 maximum 100 step 0.01
+    }
+    Fl_Button {} {
+      label {ins Messgerät schreiben}
+      callback {try
+ {
+  liballuris al;
+
+  cout << "ttt_device::init clear_RX" << endl;
+  al.clear_RX (500);
+  cout << "ttt_device::init stop streaming and measurement" << endl;
+  al.set_cyclic_measurement (0, 19);
+  al.stop_measurement ();
+
+  double scale = 1.0 / pow(10, al.get_digits ());
+  al.set_mode (LIBALLURIS_MODE_PEAK);
+  al.set_memory_mode (LIBALLURIS_MEM_MODE_SINGLE);
+  al.set_unit (LIBALLURIS_UNIT_N);
+
+  al.set_peak_level (vi_peak_level->value() / 100.0);
+  al.set_upper_limit (round (vi_upper_limit->value () / scale));
+  al.set_lower_limit (round (vi_lower_limit->value () / scale));
+
+  }
+  catch (std::runtime_error &e)
+  {
+    fl_alert (e.what ());
+  }}
+      xywh {150 190 205 35}
+    }
+    Fl_Button {} {
+      label {3%}
+      callback {vi_upper_limit->value (vi_nominal->value () * 1.03);
+vi_lower_limit->value (vi_nominal->value () * 0.97);}
+      xywh {290 83 45 25}
+    }
+    Fl_Button {} {
+      label {6%}
+      callback {vi_upper_limit->value (vi_nominal->value () * 1.06);
+vi_lower_limit->value (vi_nominal->value () * 0.94);}
+      xywh {340 83 45 25}
+    }
+    Fl_Value_Input vi_peak_level {
+      label {Detektionsschwelle [%]}
+      xywh {210 130 55 25} minimum 10 maximum 90 step 10 value 20
+    }
+    Fl_Button {} {
+      label {Gerätespeicher auslesen}
+      callback {try
+{
+  liballuris al;
+
+  al.stop_measurement ();
+  double scale = 1.0 / pow(10, al.get_digits ());
+
+  vector<int> mem;
+  mem = al.read_memory ();
+
+  cout << "mem.size ()=" << mem.size () << endl;
+
+  if (mem.size () == 0)
+    fl_message ("Keine Werte im Gerätespeicher");
+  else
+    {
+      double sum = 0;
+      for (int k=0; k<mem.size (); ++k)
+       {
+         cout << "mem k=" << k << " =" << mem[k] << endl;
+         quick_tbl->add_measurement (mem[k] * scale);
+         sum += mem[k] * scale;
+        }
+     vo_mean->value (sum / mem.size ());
+     quick_tbl->show ();
+     }
+  }
+  catch (std::runtime_error &e)
+  {
+    fl_alert (e.what ());
+  }}
+      xywh {310 285 205 35}
+    }
+    Fl_Button {} {
+      label {Gerätespeicher löschen}
+      callback {try
+{
+  liballuris al;
+  al.clear_memory ();
+  quick_tbl->hide ();
+  }
+  catch (std::runtime_error &e)
+  {
+    fl_alert (e.what ());
+  }}
+      xywh {310 335 205 35}
+    }
+    Fl_Table quick_tbl {open
+      xywh {15 285 195 340}
+      class quick_check_table
+    } {}
+    Fl_Value_Output vo_mean {
+      label {Mittelwert [Nm]}
+      xywh {435 395 80 30} step 0.01
+    }
+  }
+  code {quick_tbl->hide ();} {}
+} 
