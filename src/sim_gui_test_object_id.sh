bb39dad5b708a926e3bbea9a2feb7a0fc39af310
Andreas Weber <weber@alluris.de>
2016-06-13 09:22:37 +0200

    add generic wrapper script to call simulation files

---
+++ src/sim_gui_test_object_id.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+fn=../test/create_test_signal/test_object_id$1.log
+echo "Running ttt_gui in sumulation mode with id = $1"
+
+if [ -e $fn ]; then
+  ./ttt_gui $1 $fn
+else
+  echo "File $fn not found! Aborting..."
+fi
+
+
--- src/sim_gui_test_object_id8.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-./ttt_gui 8 ../test/create_test_signal/test_object_id8.log
-
